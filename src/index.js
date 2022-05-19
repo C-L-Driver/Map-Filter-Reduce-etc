1688b7f640ca9fdd182c410a65f3e4d88161c820
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-05-19 20:48:18 +0100

    also arrow functions practice

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
-//Filter - Create a new array by keeping the items that return true.
+// const newNumber = numbers.map(x => x* x)
+
+// console.log(newNumber)
+
+// //Filter - Create a new array by keeping the items that return true.
+
+// const newNumber2 = numbers.filter(x => x > 5)
+
+// console.log(newNumber2)
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+// const newNumber2 = numbers.reduce(
+//   (accumulator, currentNumber) => (accumulator += currentNumber)
+// );
+
+// console.log(newNumber2);
+
 //Find - find the first item that matches from an array.
 
+// const newNumber2 = numbers.find((x) => x > 2);
+
+// console.log(newNumber2);
+
 //FindIndex - find the index of the first item that matches.
+
+// const newNumber2 = numbers.findIndex((x) => (x = 56));
+
+// console.log(newNumber2);
+
+// import emojipedia from "./emojipedia";
+
+// const meaningClip100 = emojipedia.map(function (emojiEntry) {
+//   return emojiEntry.meaning.substring(0, 100);
+// });
+
+// console.log(meaningClip100);
